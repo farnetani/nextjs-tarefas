540b2bcad327ebcb12969a9cb1ac99b564f25e77
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-08-08 17:50:44 -0300

    :sparkles: create class model tarefa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
+import Tarefa from "../model/Tarefa"
+
 export default function Home() {
+
+ let tarefa: Tarefa = new Tarefa(1, 'Exemplo de Tarefa')
+  // tarefa = tarefa.concluir()
+  tarefa = tarefa.alternarStatus()
+
   return (
     <div className={`
-      flex flex-col
-      justify-center
-      bg-purple-600
-      h-screen
+      flex flex-col justify-center items-center h-screen
+      text-white bg-purple-600 bg-gradient-to-tr from-purple-500 to-yellow-600      
     `}>
-      <span>1</span>
-      <span>2</span>
-      <span>3</span>
+      <span>{ tarefa.id }</span>
+      <span>{ tarefa.descricao }</span>
+      <span>{ tarefa.concluida ? 'Concluída' : 'Ativa' }</span>
     </div>
   )
 }
