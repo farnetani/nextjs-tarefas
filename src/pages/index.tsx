4c1db57fed20d97755ba3da1130c9924a003f6b7
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-08-08 17:25:58 -0300

    :tada: NextJS with typescript and tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className={`
+      flex flex-col
+      justify-center
+      bg-purple-600
+      h-screen
+    `}>
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  )
+}
